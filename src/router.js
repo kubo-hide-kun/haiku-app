5d4ebc347378f1c71067eb5717465217241f9a24
nakano <tohokamiemitame4185@gmail.com>
2019-08-23 21:22:09 +0900

    リファクタ

--- src/router.js
+++ src/router.js
@@ -5,34 +5,21 @@ import Router from 'vue-router'
 Vue.use(Router)
 
 export default new Router({
-  mode: 'history',
-  //Routerを使用するURL
-  base: process.env.BASE_URL,
-  //URL一覧
-  routes: [
-    {//about
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (about.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
-    },
-    {//form
-      path: '/form',
-      name: 'form',
-      component: () => import('./views/form.vue')
-    },
-    {//rain
-      path: '/main',
-      name: 'main',
-      component: () => import('./views/main.vue')
-    },
-    {
-      path: '/send',
-      name: 'send',
-      component: () => import('./views/send.vue')
-    },
+    mode: 'history',
+    //Routerを使用するURL
+    base: process.env.BASE_URL,
+    //URL一覧
+    routes: [
+        {//form
+            path: '/kukai',
+            name: 'kukai',
+            component: () => import('./views/kukai.vue')
+        },
+        {
+            path: '/send',
+            name: 'send',
+            component: () => import('./views/send.vue')
+        },
 
-  ]
+    ]
 })
---
+++ src/views/kukai.vue
@@ -0,0 +1,49 @@
+<template>
+    <div class="kukai">
+        <center>
+            <section class="tategaki">たてがき</section>
+        </center>
+
+        <!--<button class="fab" type="submit" @click="onsubmit"><img src="../assets/hude.png" width="60" height="60"></button>-->
+
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'kukai',
+    }
+</script>
+<style scoped>
+    .kukai {
+        top: 15%;
+        left: 50%;
+        height: 60%;
+        transform: translateX(-50%);
+        z-index: 60;
+        position: absolute;
+        background-color: #FFFFFF;
+        border: solid 10px #1D5329;
+        box-shadow: 5px 2px 0px 0px rgba(0, 0, 0, 0.8);
+    }
+
+    .fab {
+        position: fixed;
+        right: -100px;
+        bottom: -70px;
+        transition: all 0.1s ease-in-out;
+    }
+
+    .fab:hover {
+        box-shadow: 0 6px 14px 0 #666;
+        transform: scale(1.05);
+    }
+
+    .tategaki {
+        -webkit-writing-mode: vertical-rl;
+        -ms-writing-mode: tb-rl;
+        writing-mode: vertical-rl;
+        font-size: 40px;
+        font-family: "KouzanBrushFontSousyoOTF", "游明朝体+36ポかな", "游明朝体";
+    }
+</style>
