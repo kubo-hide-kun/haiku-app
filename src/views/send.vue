ef88cdf624d44177bf4e500cdc3067485ccf6038
nakano <tohokamiemitame4185@gmail.com>
2019-08-24 03:27:16 +0900

    季語と画像をアップロードする吟行を実装

--- src/views/send.vue
+++ src/views/send.vue
@@ -1,25 +1,40 @@
 <template>
-    <div class="send">
-        <div class="input-word">
-            単語: <input v-model="haiku"/>
+    <div class="outer">
+        <!--        <div class="input-word">-->
+        <!--            単語: <input v-model="kigo"/>-->
+        <!--        </div>-->
+
+        <!--        <div class="input-img">-->
+        <!--            <label class="input-item__label" v-show="!uploadedImage">-->
+        <!--                画像を選択-->
+        <!--                <input type="file" @change="onFileChange"/>-->
+        <!--            </label>-->
+        <!--        </div>-->
+
+        <!--&lt;!&ndash;        <div class="show-img" v-show="uploadedImage">&ndash;&gt;-->
+        <!--&lt;!&ndash;            <img :src="uploadedImage"/>&ndash;&gt;-->
+        <!--&lt;!&ndash;        </div>&ndash;&gt;-->
+
+
+        <!--        <div v-show="uploadedImage" @click="remove" key="send-img"><p>{{img_name}} x </p></div>-->
+        <!--        <div v-show="uploadedImage && kigo">-->
+        <!--            <button>投句</button>-->
+        <!--        </div>-->
+        <button class="ginko" @click="upload">吟行</button>
+
+        <div class="send">
+
+
+            <div class="input-kigo">
+                <input v-model="kigo">
+            </div>
+            <div class="input-image">
+                <div>
+                    <input type="file" @change="onFileChange">
+                </div>
+            </div>
         </div>
 
-        <div class="input-img">
-            <label class="input-item__label" v-show="!uploadedImage">
-                画像を選択
-                <input type="file" @change="onFileChange"/>
-            </label>
-        </div>
-
-        <div class="show-img" v-show="uploadedImage">
-            <img :src="uploadedImage"/>
-        </div>
-
-
-        <div v-show="uploadedImage" @click="remove" key="send-img"><p>{{img_name}} x </p></div>
-        <div v-show="uploadedImage && haiku">
-            <button>投句</button>
-        </div>
     </div>
 </template>
 
@@ -30,22 +45,36 @@
             return {
                 uploadedImage: false,
                 img_name: '',
-                haiku: ''
+                kigo: '',
+                image: null
             };
         },
         methods: {
             onFileChange(e) {
                 const files = e.target.files || e.dataTransfer.files;
-                this.createImage(files[0]);
-                this.img_name = files[0].name;
+                // this.createImage(files[0]);
+                // this.img_name = files[0].name;
+                this.image = files[0]
             },
-            createImage(file) {
-                let reader = new FileReader();
-                reader.onload = e => {
-                    this.uploadedImage = e.target.result;
+            // createImage(file) {
+            //     let reader = new FileReader();
+            //     reader.onload = e => {
+            //         this.uploadedImage = e.target.result;
+            //
+            //     };
+            //     reader.readAsDataURL(file);
+            // },
+            upload() {
+                let formdata = new FormData()
+                formdata.append('image', this.image)
 
-                };
-                reader.readAsDataURL(file);
+                let params = new URLSearchParams()
+                params.append('kigo', this.kigo)
+
+                fetch('http://127.0.0.1:5042/upload' + '?' + params.toString(), {
+                    method: 'POST',
+                    body: formdata
+                }).then(r => r.json()).then(j => console.log(j))
             },
             remove() {
                 this.uploadedImage = '';
@@ -54,15 +83,92 @@
     };
 </script>
 <style scoped>
+    /*.send {*/
+    /*    display: flex;*/
+    /*    flex-direction: column;*/
+    /*    height: 100%;*/
+    /*    border:solid 10px black;*/
+
+
+    /*}*/
+
+    .outer {
+
+    }
+
+    .ginko {
+        top: 4%;
+        right: 5%;
+        position: absolute;
+        z-index: 60;
+        background: #FFF; /*ボタン色*/
+        color: #222;
+        border-radius: 3px;
+        padding: 4px;
+        -webkit-writing-mode: vertical-rl;
+        -ms-writing-mode: tb-rl;
+        writing-mode: vertical-rl;
+        font-family: "KouzanBrushFontSousyoOTF", "游明朝体+36ポかな", "游明朝体";
+    }
+
+
     .send {
-        top: 20%;
+        top: 12%;
         left: 50%;
         transform: translateX(-50%);
         z-index: 60;
         position: absolute;
+        height: 75%;
+        width: 100%;
+
+        display: flex;
+        flex-direction: column;
     }
 
-    label > input {
-        display: none;
+
+    .input-kigo {
+        color: red;
+        height: 50%;
+        display: flex;
+        align-items: center;
     }
+
+    .input-kigo > input {
+        width: 100%;
+        height: 80%;
+        /*background-color: rgba( 204, 255, 204, 0 );*/
+
+        text-align: center;
+        font-size: 20px;
+    }
+
+    .input-image {
+        height: 50%;
+        display: flex;
+        align-items: center;
+    }
+
+    .input-image > div {
+        width: 100%;
+        height: 80%;
+        display: flex;
+        flex-direction: column; /* 子要素をflexboxにより縦方向に揃える */
+        justify-content: center; /* 子要素をflexboxにより中央に配置する */
+        align-items: center; /* 子要素をflexboxにより中央に配置する */
+        background-color: #FFFFFF;
+    }
+
+    .input-image > div > input {
+        width: 100%;
+        height: 100%;
+        background: url(../assets/ginkou_on.png) center no-repeat;
+
+    }
+
+    /*.input-image >*/
+
+
+    /*label > input {*/
+    /*    display: none;*/
+    /*}*/
 </style>
